0c586ae61d3f30c9266dfdb22c72a070dd9bd955
Nirmal <vilify.nirmal@gmail.com>
2020-11-01 12:42:38 +0530

    signup with form validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:blogapp/welcome.dart';
+import 'package:blogapp/pages/welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: WelcomePage(),
     );
---
+++ lib/pages/SignUpPage.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool vis = true;
+  final _globalkey = GlobalKey<FormState>();
+  Widget usernameTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Username"),
+          TextFormField(
+            validator: (value) {
+              if (value.isEmpty) {
+                return "UserName cant be empty";
+                //todo unique or not
+              }
+              return null;
+            },
+            // controller: _usernameController,
+            decoration: InputDecoration(
+              // errorText: validate ? null : errorText,
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget emailTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Email"),
+          TextFormField(
+            // controller: _emailController,
+            validator: (value) {
+              if (value.isEmpty) return "Email can't be empty";
+              if (!value.contains("@")) return "Email is Invalid";
+              return null;
+            },
+            decoration: InputDecoration(
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget passwordTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Password"),
+          TextFormField(
+            // controller: _passwordController,
+            validator: (value) {
+              if (value.isEmpty) return "Password can't be empty";
+              if (value.length < 8) return "Password lenght must have >=8";
+              return null;
+            },
+            obscureText: vis,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(vis ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    vis = !vis;
+                  });
+                },
+              ),
+              helperText: "Password length should have >=8",
+              helperStyle: TextStyle(
+                fontSize: 14,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: [Colors.white, Colors.green[200]],
+          begin: const FractionalOffset(0.0, 1.0),
+          end: const FractionalOffset(0.0, 1.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.repeated,
+        )),
+        child: Form(
+          key: _globalkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign up with email",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              usernameTextField(),
+              emailTextField(),
+              passwordTextField(),
+              SizedBox(
+                height: 20,
+              ),
+
+              //for kaam chalau signup button
+              InkWell(
+                onTap: () {
+                  if (_globalkey.currentState.validate()) {
+                    //if success we will send data to rest server
+                    print("Validated!");
+                  }
+                },
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xff00A86B),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+              // emailTextField(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
